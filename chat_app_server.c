f31660d5e767bcfb37703de78f43af21e2d3efa5
Abhishek Gupta <abhishekgupt34@gmail.com>
2017-02-01 23:26:00 +0530

    Rudimentary chat, alternate text messages

---
+++ chat_app_server.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h> 
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+//Server file
+
+int main(int argc,char* arg[]){
+	
+	if (argc<2){
+		printf("Usage : Program [Port]\n");
+		return 0;
+	}
+	int port_no = atoi(arg[1]);
+	int server_socket=0,new_socket=0;
+	
+	server_socket = socket(AF_INET, SOCK_STREAM,0);
+	if (server_socket==-1){
+		perror("Failed to create socket");
+		return 0;
+	}
+
+	/*Attach server address to socket*/
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port_no);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); /*Read upon localhost*/
+	memset(serverAddr.sin_zero,'\0', sizeof(serverAddr.sin_zero));
+	char buffer[1024];
+	memset(buffer,0,sizeof(buffer));
+	/*Note :- type of &serverAddr is typecasted to (struct sockaddr)* */
+	if (bind(server_socket, (struct sockaddr *) &serverAddr, sizeof(serverAddr))==-1){
+		perror("Failed to bind address to socket");
+		return 0;
+	}
+	if (listen(server_socket,5) != 0){
+		perror("Error in listening");
+		return 0;
+	}
+	
+	struct sockaddr_storage client_addr;
+	int sz;
+	sz = sizeof(client_addr);
+	new_socket = accept(server_socket,(struct sockaddr *)&client_addr, &sz);
+	strcpy(buffer,"Welcome to hop world!\n");
+	send(new_socket,buffer,strlen(buffer)+1,0);
+	while (1) {
+		recv(new_socket,buffer,sizeof(buffer),0);
+		printf("Client : %s\n", buffer);
+		printf("Server : ");
+//		getline(&buffer,&sz,stdin);
+		scanf("%s",buffer);
+		send(new_socket,buffer,strlen(buffer)+1,0);
+	}
+	return 0;
+}
---
+++ client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+
+//Client Program
+
+int main(int argc,char* arg[]){
+	if (argc<3){
+		printf("Usage : Program [IP ADDRESS] [PORT]\n");
+		return 0;
+	}
+	int port_no = atoi(arg[2]);
+	int socket_no = socket(AF_INET,SOCK_STREAM,0);
+	struct sockaddr_in serverAddr;
+	int addr_sz = 0;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port_no);
+	serverAddr.sin_addr.s_addr = inet_addr(arg[1]);
+	memset(serverAddr.sin_zero,'\0',sizeof(serverAddr.sin_zero));
+	addr_sz = sizeof(serverAddr);
+	connect(socket_no,(struct sockaddr *)&serverAddr,addr_sz);
+	char buffer[1024];
+	int sz;
+	while (1){
+		recv(socket_no,buffer,sizeof(buffer),0);	
+		printf("Server : %s\n",buffer);
+		printf("Client : ");
+		//getline(&buffer,&sz,stdin);
+		scanf("%s",buffer);
+		send(socket_no,buffer,strlen(buffer)+1,0);
+	}
+	return 0;
+}
+	
